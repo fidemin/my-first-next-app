fd56ddf68826f71d3647d05e1f1cd16fa31696ad
Yunhong Min <yunongmin@gmail.com>
2021-08-11 23:57:47 +0900

    first commit

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+
+function AboutPage() {
+    return <div>
+        <h1>This is about page!</h1>
+        <a href="./">return</a>
+    </div>
+}
+
+export default AboutPage
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+
+function HomePage() {
+    return <div>
+        <h1>Welcome to Next.js</h1>
+        <a href="./about">To about page</a>
+
+    </div>
+}
+
+export default HomePage
