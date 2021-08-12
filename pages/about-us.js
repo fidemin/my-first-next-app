8df71d7975042cb2cffb3e5e26c007770f5b4327
Yunhong Min <yunongmin@gmail.com>
2021-08-12 20:42:53 +0900

    abount-us page

---
+++ pages/about-us.js
@@ -0,0 +1,8 @@
+
+export default function AboutUsPage() {
+    return <div>
+        <h1>About us</h1>
+        <h2>This is my first react and next js web application</h2>
+        <a href="/">return</a>
+    </div>
+}
--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,8 @@
 function HomePage() {
     return <div>
         <h1>Welcome to Next.js</h1>
-        <a href="./about">To about page</a>
-
+        <div><a href="/products">To products</a></div>
+        <div><a href="/about-us">To about page</a></div>
     </div>
 }
 
